f9a1e4fc9647597901d9dc02285cd8bb9d337c86
Saad Khalid <saadkhalid90@gmail.com>
2022-01-12 00:08:48 +0500

    chapter title pages developed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
 import './App.css';
+import ChapterTitle from './components/ChapterTitle.js';
+
+import chapter1_1 from './resources/Chapter1/Chapter1_1.png';
+import chapter1_2 from './resources/Chapter1/Chapter1_2.png';
+import chapter1_3 from './resources/Chapter1/Chapter1_3.png';
+
+import chapter2_1 from './resources/Chapter2/Chapter2_1.jpg';
+import chapter2_2 from './resources/Chapter2/Chapter2_2.jpg';
+import chapter2_3 from './resources/Chapter2/Chapter2_3.jpg';
+
+import chapter3_1 from './resources/Chapter3/Chapter3_1.jpg';
+import chapter3_2 from './resources/Chapter3/Chapter3_2.jpg';
+import chapter3_3 from './resources/Chapter3/Chapter3_3.jpg';
+
+const imgArrCh1 = [
+  {
+    image: chapter1_1,
+    style: {
+      width: '20%'
+    }
+  },
+  {
+    image: chapter1_2,
+    style: {
+      width: '50%'
+    }
+  },
+  {
+    image: chapter1_3,
+    style: {
+      width: '20%'
+    }
+  }
+];
+
+const imgArrCh2 = [
+  {
+    image: chapter2_1,
+    style: {
+      width: '25%'
+    }
+  },
+  {
+    image: chapter2_2,
+    style: {
+      width: '50%'
+    }
+  },
+  {
+    image: chapter2_3,
+    style: {
+      width: '25%'
+    }
+  }
+];
+
+const imgArrCh3 = [
+  {
+    image: chapter3_1,
+    style: {
+      width: '25%'
+    }
+  },
+  {
+    image: chapter3_2,
+    style: {
+      width: '50%'
+    }
+  },
+  {
+    image: chapter3_3,
+    style: {
+      width: '25%'
+    }
+  }
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        /*
+        <ChapterTitle
+          number="2"
+          imgArr={imgArrCh2}
+          title="National Polls"
+          subtitle="1970"
+          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        />
+        <ChapterTitle
+          number="3"
+          imgArr={imgArrCh3}
+          title="Military Operation"
+          subtitle="March 25, 1971 - ..."
+          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        />
+        <ChapterTitle
+          number="1"
+          imgArr={imgArrCh1}
+          title="Background"
+          subtitle="1947 - 1969"
+          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        />
+        */
+        <ChapterTitle
+          number="1"
+          imgArr={imgArrCh1}
+          title="Background"
+          subtitle="1947 - 1969"
+          desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+        />
+      }
     </div>
   );
 }
---
+++ src/components/ChapterTitle.js
@@ -0,0 +1,35 @@
+import styles from '../css-modules/chapter-title.module.css';
+import mujeebBG from '../resources/MujeebBG.jpg';
+
+import lumsLogo from '../resources/nav/LumsArchiveLogo.png';
+import hambMenu from '../resources/nav/hamburger.png';
+
+const titleStyle = {
+  background: `linear-gradient(to bottom, #FFFFFFFF 5%, #FFFFFFDE 75%, #FFFFFFF7 100%), url(${mujeebBG}) no-repeat left top`,
+  backgroundSize: `cover`
+}
+
+
+function ChapterTitle({number, imgArr, title, subtitle, desc}){
+  return (
+    <header className={styles.chapter_title} style={titleStyle}>
+      <nav className={styles.navbar}>
+        <img className={styles.logo} src={lumsLogo}/>
+        <img className={styles.menu} src={hambMenu}/>
+      </nav>
+      <div className={styles.chapter_title_content}>
+        <p className={styles.chapter_no}>Chapter - <span>{number}</span></p>
+        <div className={styles.img_contain}>
+          {imgArr.map((image, imgIdx) => (
+            <img src={image.image} key={`image${imgIdx}`} className={styles[`img${imgIdx+1}`]}></img>
+          ))}
+        </div>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.subtitle}>{subtitle}</p>
+        <p className={styles.desc}>{desc}</p>
+      </div>
+    </header>
+  )
+}
+
+export default ChapterTitle;
