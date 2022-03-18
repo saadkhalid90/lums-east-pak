e850022f297932c4f7cfaba1f3debc0f4e897e08
Hasan Khalid <hasankhalid940@gmail.com>
2022-03-18 02:27:29 -0400

    add fade animations to chapter title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import preloadImage from './resources/navBG.png';
+import { useEffect } from 'react';
+
 
 import {
   BrowserRouter as Router,
@@ -15,7 +17,35 @@ import Chapter4 from './views/bloodtele.js';
 import Chapter5 from './views/race.js';
 import LocalNews from './views/localNews.js';
 
+import chapter1_1 from "./resources/Chapter1/Chapter1_1.png";
+import chapter1_2 from "./resources/Chapter1/Chapter1_2.png";
+import chapter1_3 from "./resources/Chapter1/Chapter1_3.png";
+import chapter4_1 from "./resources/Chapter4/Chapter4_1.jpg";
+import chapter4_2 from "./resources/Chapter4/Chapter4_2.jpg";
+import chapter4_3 from "./resources/Chapter4/Chapter4_3.jpg";
+import chapter5_1 from "./resources/Chapter5/Chapter5_1.jpg";
+import chapter5_2 from "./resources/Chapter5/Chapter5_2.jpg";
+import chapter5_3 from "./resources/Chapter5/Chapter5_3.jpg";
+import chapter3_1 from "./resources/Chapter3/Chapter3_1.jpg";
+import chapter3_2 from "./resources/Chapter3/Chapter3_2.jpg";
+import chapter3_3 from "./resources/Chapter3/Chapter3_3.jpg";
+import chapter2_1 from "./resources/Chapter2/Chapter2_1.jpg";
+import chapter2_2 from "./resources/Chapter2/Chapter2_2.jpg";
+import chapter2_3 from "./resources/Chapter2/Chapter2_3.jpg";
+import landing_1 from './resources/Home/Land1.jpg';
+import landing_2 from './resources/Home/Land2.jpg';
+import landing_3 from './resources/Home/Land3.jpg';
+import landing_4 from './resources/Home/Land4.jpg';
+import landing_5 from './resources/Home/Land5.jpg';
+
 function App() {
+  useEffect(() => {
+    const imageArr = [chapter1_1, chapter1_2, chapter1_3, chapter4_1, chapter4_2, chapter4_3, chapter5_1, chapter5_2, chapter5_3, chapter3_1, chapter3_2, chapter3_3, chapter2_1, chapter2_2, chapter2_3, landing_1, landing_2, landing_3, landing_4, landing_5];
+    imageArr.forEach((image) => {
+      new Image().src = image
+    });
+  }, []);
+
   return (
     <div className="App">
       <img src={preloadImage} style={{display: 'none'}}></img>
